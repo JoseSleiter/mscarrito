2528058fc22ddf4d72d7b1d8d76c7c67475ccc4b
jose sleiter rios <josesleiter@gmail.com>
2019-08-06 03:09:20 -0400

    feat: Añadir variables de entorno

--- api/index.js
+++ api/index.js
@@ -1,18 +1,21 @@
 'use strict'
 
+require('./src/config/config')
 var mongoose = require('mongoose');
 var app = require('./src/app');
 
-// Database
-// mongoose.Promise = global.Promise;
-// mongoose.connect('mongodb://f6507777b550:27017/local', { useNewUrlParser: true})
-//     .then(() => {
-//         console.log("La conexión a la base de datos local se ha realizado correctamente")        
-        // CREAR EL SERVIDOR WEB CON NODEJS
-        app.listen( app.get('port'), () => {
-            console.log(`Server started on port ${app.get('port')}`);
-            // console.log(mongoose.get('test'))
+// Database mlab.com
+mongoose.Promise = global.Promise;
 
-        });
-    // })
-    // .catch(err => console.log(err));
+// Conexion
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true})
+.then(() => {
+    console.log("La conexión a la base de datos local se ha realizado correctamente")        
+    // CREAR EL SERVIDOR WEB CON NODEJS
+    app.listen( process.env.PORT, () => {
+        console.log(`Server started on port ${ process.env.PORT}`);
+        // console.log(mongoose.get('test'))
+
+    });
+})
+.catch(err => console.log(err));
--- api/src/app.js
+++ api/src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 // set variables
-app.set('port', '3000');
+// app.set('port', '3000');
 app.set('json spaces', 2)
 
 // middlewares
